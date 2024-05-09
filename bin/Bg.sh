481404bf803cefb27e2ff563aba7fb9ee8da95ba
Pegasis1002 <thewebserfer1234@gmail.com>
2024-05-10 01:24:15 +0530

    last on pc| 2024.4.10

--- bin/Bg.sh
+++ bin/Bg.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-wall=$(find ~/wallpapers -name "*.jpg" -o -name "*.png" | shuf -n 1)
+# generate a random number between 0 and 9
+rand=$((($RANDOM % 10)))
+
+if [[ $rand > 5 ]]; then
+	wall=$(find ~/wallpapers/misc -name "*.jpg" -o -name "*.png" | shuf -n 1)
+else
+	wall=$(find ~/wallpapers/landscapes -name "*.jpg" -o -name "*.png" | shuf -n 1)
+fi
 
 echo $wall
 feh --bg-fill $wall
--- install.sh
+++ install.sh
@@ -3,57 +3,45 @@
 
 path=$(pwd)
 
+mv $HOME/.bashrc $path/backup
+cp $path/.bashrc $HOME/.bashrc
+
 # check if bspwm && sxhkd && kitty && picom are installed
-if ! [ -x "$(command -v bspwm)" ] || ! [ -x "$(command -v sxhkd)" ] || ! [ -x "$(command -v kitty)" ] || ! [ -x "$(command -v picom)" ]; then
-{
+if ! [ -x "$(command -v bspwm)" ] || ! [ -x "$(command -v sxhkd)" ] || ! [ -x "$(command -v kitty)" ] || ! [ -x "$(command -v picom)" ]; then{
   echo "Installing dependencies..."
   sudo pacman -S bspwm sxhkd kitty picom
-}
-fi
+}fi
 
 # check if bin directory exists
-if [ -d "$HOME/.local/bin" ]; then
-{
-  cp $path/bin/ $HOME/.local/bin
+if [ -d "$HOME/.local/bin" ]; then{
+  cp -r $path/bin/ $HOME/.local/bin
 }else{
   mkdir -p $HOME/.local/bin
-  cp $path/bin/ $HOME/.local/bin
-}
-fi
+  cp -r $path/bin/ $HOME/.local/bin
+}fi
 
 #check if the directories exists
-if [ -d "$HOME/.config/bspwm/" ]; then
-{
+if [ -d "$HOME/.config/bspwm/" ]; then{
   mv $HOME/.config/bspwm/ $path/backup
-}else
-  cp $path/bspwm/ $HOME/.config/bspwm/
-fi
+}else{
+  cp -r $path/bspwm/ $HOME/.config/bspwm/
+}fi
 
-if [ -d "$HOME/.config/sxhkd/" ]; then
-{
+if [ -d "$HOME/.config/sxhkd/" ]; then{
   mv $HOME/.config/sxhkd/ $path/backup
-}else
-  cp $path/sxhkd/ $HOME/.config/sxhkd/
-fi
+}else{
+  cp -r $path/sxhkd/ $HOME/.config/sxhkd/
+}fi
 
-if [ -d "$HOME/.config/kitty/" ]; then
-{
+if [ -d "$HOME/.config/kitty/" ]; then{
   mv $HOME/.config/kitty/ $path/backup
-}else
-{
-  cp $path/kitty/ $HOME/.config/kitty/
-}
-fi
+}else{
+  cp -r $path/kitty/ $HOME/.config/kitty/
+}fi
 
-if [ -d "$HOME/.config/picom/" ]; then
-{
+if [ -d "$HOME/.config/picom/" ]; then{
   mv $HOME/.config/picom/ $path/backup
 }else{
-  cp $path/picom/ $HOME/.config/picom/
-}
-fi
+  cp -r $path/picom/ $HOME/.config/picom/
+}fi
 
-mv $HOME/.bashrc $path/backup
-cp $path/bashrc $HOME/.bashrc
-
-echo "Done!"
